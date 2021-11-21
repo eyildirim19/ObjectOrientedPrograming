af3e079efda3e7f724c20b8b2e8298b24d4d9b21
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2021-11-21 13:45:12 +0300

    Encapsulation ve Property

---
+++ Property/Ogrenci.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Property
+{
+    public class Ogrenci
+    {
+        /*         
+        Sınıf üyeleri;
+
+        field =  değer tutan program varlığı
+        metot = işlem yapan program varlığı
+        properties = c# 3.0 ile gelmiştir... field ve metot karışımıdır...Bundan sonraki süreçte gerekmedikçe field kullanmayacağız...property filed görevi görürler... propertyler içerisinde get ve set metodu barından özel varlıklardır. get ve set metodu property'inin işaret ettiği fielda değer atar, değer okur.
+         */
+
+        //public string Adi { get; set; } // property...
+
+        // propertyi anlamak için encapsulation ve get ve set metotları anlamak gerekir...
+
+        // Encapsulation => üyeler içeriye açık dışarıya kapalı..
+        private int vize;
+        public void SetVize(int n) // metodun tek bir görevi var sadece değer set etmek. bu yüzden geri dönüş tipi voiddir.
+        {
+            if (n >= 0 && n <= 100)
+            {
+                vize = n;
+            }
+        }
+
+        public int GetVize() // metodun görevi geriye saklanan notu dönmek olduğıu için dönüş tipi inttir...
+        {
+            return vize;
+        }
+
+        // Encapsulation'ı property ile yapıyorz...
+        private int final;
+        public int Final
+        {
+            get // gizli bir mettotur. geriye property tipinde değer döner
+            {
+                return final;
+            }
+            set // gizli bir void metottur. parametre alır. parametre value'dan gelir..
+            {
+                if (value >= 0 && value <= 100)
+                {
+                    final = value;
+                }
+            }
+        }
+    }
+}
---
+++ Property/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Property
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // öğrenci notu 0 ile 100 arasında girilebilir...bu değerleri alabilmek için nesnin notunu set ederken kontrol yapmak gerekir...
+
+            // Not : private olduğu için buradan değer atanamaz
+            Ogrenci ogr = new Ogrenci();
+            // ogr.Not = 200;
+            ogr.SetVize(200);
+            ogr.Final = 200;
+
+            Ogrenci ogr1 = new Ogrenci();
+            // ogr1.Not = -1;
+            ogr1.SetVize(-1);
+            ogr1.Final = -1;
+
+            Ogrenci ogr2 = new Ogrenci();
+            // ogr2.Not = 100;
+            ogr2.SetVize(100);
+            ogr2.Final = 100;
+
+            Console.WriteLine($"Girilen Not= {ogr.GetVize()} Fİnal = {ogr.Final}");
+            Console.WriteLine($"Girilen Not= {ogr1.GetVize()} Fİnal = {ogr1.Final}");
+            Console.WriteLine($"Girilen Not= {ogr2.GetVize()} Fİnal = {ogr2.Final}");
+
+            // todo:  Encapsulation nedir araştırınız. döküman şeklinde hazırlayıp github'a ekleyiniz..
+
+            Console.ReadKey();
+        }
+    }
+}
