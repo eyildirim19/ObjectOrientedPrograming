16ae02f104c500f4a48a8fb3c4523fd0f62cb56f
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2021-11-21 11:38:08 +0300

    Add project files.

---
+++ Classses/Ogrenci.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classses
+{
+    #region tanim
+    // class içerisindeki değişken, metot gibi varlıklara class member denir (sınıf üyeleri)... üyeler class seviyesi varlıklardır.. bu üyelerin en temeli değişkenlerdir. bu değişkenlere field denir.. fieldlar sınıf içerisinde verir tutmak için, metotlar işlem yapmak için kullanılır...ama en temel üye herzaman fieldlardır..Sınıf üyeleri oluşturulduğunda default olarak sınıf için oluşturulur. derleyici bunu böyle kabul eder.bizler sınıf üyelerini sınıf dışına açmamız gerekir...
+
+    // sınıf üyelerinin nereden erişilebilir olacağına accsess modifiers (erişim belirleyiciler) ile karar verilir...Bunlar;
+    // private, protected, internal, protected internal, public'tir.En sık kullandıklarımız private(özel) ve public (genel).
+    // Not : sınıf üyelerinin erişim seviyeleri default private'dır.
+
+    // Ogrenci sınıfı bizim için uygulama tarafında kullanılacak bir veri tiptir.
+
+    // Classlar soyut varlıklardır. classlardan oluşturulan nesneler somut varlıklardır...
+    // Classlar referans tiplerdir...
+    #endregion
+
+    class Ogrenci
+    {
+        public string adiSoyAdi;
+        public byte yas;
+        public char cinsiyet;
+    }
+}
---
+++ Classses/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Classses
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            #region tanim
+            // Object Orinented Programming(OOP) = Nesne Yönelimli Programlama
+            // bir programlama modelidir...gündelik hayattaki varlıkların programalama ortamında modellenebilmesidir..
+
+            // Yaklaşımlar;
+            // Encapsulation (kapsülleme)
+            // Abstraction (Soyutlama)
+            // Polimorphism (Çok biçimlilik)
+
+            // Amaç;
+            // bir kere yaz, sürekli kullan... (kod tekrarnının önlenmesi...)
+
+
+            // Class (Sınıf) => Classlar uygulama içerisinde soyut varlıklar oluşturmak için kullanılan kod bloklarıdır. Amacı içerisinde birden fazla değişkeni, metodu tutmak için kullanılır. Komutlar bir sınıf içerisinde tasarlanarak modülerlik sağlanır..Örnek olarak Convert, Array, Console sınıfları. Her bir sınıf içerisinde kendiyle ilişkili komutları barındırır.. Bir sınıf oluşturmak için projeye sağ tık yapılır, add new item seçeneği ile gelen pencereden class dosyası sçeilerek projeye eklenir...
+
+            // eğer metotlar arabanın bir parçasıysa class arabanın bütünüdür..
+
+
+            // Not : class tipinde oluşturulan değişkenlere nesne(object) denir..sınıf üyerine  nesneadi. ile erişilir.. nesneler üzerinde işlem yapabilmek için nesnelerin instance (örneK) olması gerekir. instance'lar new komutu oluşturulur...
+            #endregion
+
+            Ogrenci ogrenci = new Ogrenci();
+            ogrenci.adiSoyAdi = "Furkan";
+            ogrenci.cinsiyet = 'E';
+            ogrenci.yas = 20;
+
+            Ogrenci ogrenci1 = new Ogrenci();
+            ogrenci1.adiSoyAdi = "Bahar";
+            ogrenci1.cinsiyet = 'K';
+            ogrenci.yas = 18;
+
+            Console.WriteLine(ogrenci.adiSoyAdi);
+            Console.WriteLine(ogrenci1.adiSoyAdi);
+
+            Console.ReadKey();
+        }
+    }
+}
