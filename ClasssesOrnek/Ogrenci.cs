ff07a3290f2e4841356b72da2453a2a2eea4392f
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2021-11-21 12:08:00 +0300

    parametre ile metodu tekrar çalışmasını önlemek...

---
+++ ClasssesOrnek/Ogrenci.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasssesOrnek
+{
+    // Sınıfların default erişim seviyeleri internal'dır..
+    // private ve protected aynı seviyede
+    // internal ve public aynı seviyedir. (ikiside geneldir. internal proje içerisinde public her yerde(farklı projelerde) erişilebilir..
+    public class Ogrenci
+    {
+        public string adi;
+        public float vize;
+        public float final;
+
+        // not : sınıf üyesi fieldlara sınıf seviyesinde erişilemez..üyelere erişmek için metot veya property kullanılmalıdır..
+        //public float ortalama = vize * 0.4f + final * 0.6f;
+
+        public float Ortalama()
+        {
+            return vize * 0.4f + final * 0.6f;
+        }
+
+        public string HarfNot(float deger)
+        {
+            float ort = deger;
+
+            if (ort >= 80 && ort < 100)
+                return "AA";
+            else if (ort > 60 && ort < 80)
+                return "BB";
+            else if (ort >= 40 && ort < 60)
+                return "CC";
+            else
+                return "FF";
+        }
+    }
+}
---
+++ ClasssesOrnek/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClasssesOrnek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ogr = new Ogrenci();
+            ogr.adi = "Furkan";
+            ogr.vize = 50;
+            ogr.final = 60;
+
+            Ogrenci ogr1 = new Ogrenci();
+            ogr1.adi = "Evren";
+            ogr1.vize = 40;
+            ogr1.final = 70;
+           
+            Console.WriteLine($"{ogr.adi}  = {ogr.Ortalama()} {ogr.HarfNot(ogr.Ortalama())}");
+            Console.WriteLine($"{ogr1.adi} = {ogr1.Ortalama()} {ogr1.HarfNot(ogr1.Ortalama())}");
+
+            Console.ReadKey();
+        }
+    }
+}
