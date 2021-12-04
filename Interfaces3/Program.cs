387c8db83a009194c2c015ef9e8969fb3b50a85f
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2021-12-04 11:55:48 +0300

    interface son örnek...

---
+++ Interfaces3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Interfaces3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Onder ond = new Onder();
+            Furkan frk = new Furkan();
+            Suna sn = new Suna();
+            Evren evrn = new Evren();
+            Bahar bhr = new Bahar();
+
+            // Öğrencilerin düşüncelerini main dışındaki bir metot ile gösterelim..
+            Dusunce(ond);
+            Dusunce(frk);
+            Dusunce(sn);
+            Dusunce(evrn);
+            Dusunce(bhr);
+
+
+            Console.ReadKey();
+        }
+
+        // Dusunce metodunun IOgrenci tipindeki parametresi sayesinde Yeni bir öğrenci geldiğinde bu metodu geliştirmeyeceğiz. çünkü eğer öğren IOgrenciden türerse IOgrenci o öğrenci çalışabilir...
+        static void Dusunce(IOgrenci ogrenci) // IOgrenci tipinde gönderilen instance için çalışır...
+        {
+            ogrenci.Dusunce();
+        }
+
+        #region Old
+        static void DusunceF(Furkan f)
+        {
+            f.Dusunce();
+        }
+
+        static void DusunceO(Onder o)
+        {
+            o.Dusunce();
+        }
+
+        static void DusunceS(Suna s)
+        {
+            s.Dusunce();
+        }
+
+        static void DusunceE(Evren e)
+        {
+            e.Dusunce();
+        }
+        #endregion
+    }
+}
---
+++ Interfaces3/kods.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces3
+{
+    // proje deki inteface ve kodların bulunduğu dosya...
+    public interface IOgrenci
+    {
+        void Dusunce();
+    }
+
+    // implemente etmek, interfaceden gelen üyenin miras alan sınıf tarafından tamamlanması demektir...
+    public class Furkan : IOgrenci
+    {
+        public void Dusunce()
+        {
+            Console.WriteLine("interface çok zor...");
+        }
+    }
+
+    public class Onder : IOgrenci
+    {
+        public void Dusunce()
+        {
+            Console.WriteLine("OOP genel olarak güzel");
+        }
+    }
+
+    public class Suna : IOgrenci
+    {
+        public void Dusunce()
+        {
+            Console.WriteLine("Konular fena değil");
+        }
+    }
+    public class Evren : IOgrenci
+    {
+        public void Dusunce()
+        {
+            Console.WriteLine("Uheritance ve çok biçimlikik konu");
+        }
+    }
+
+    public class Bahar : IOgrenci
+    {
+        public void Dusunce()
+        {
+            Console.WriteLine("Hoca sürekli farklı çalıştırır");
+        }
+    }
+}
